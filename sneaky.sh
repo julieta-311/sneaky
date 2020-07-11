de47f1d1acb5d2754bcdcb152e2622422bdc06c1
Julieta Bonazza <julieta@hoxtonanalytics.com>
2020-07-11 13:34:38 +0100

    Improve arguments reading/parsing
    Make the option to shoot the active window only a flag (boolean)
    and parse the rest of the arguments (integers). Document the new
    options.
    
    Only use default values for non given arguments.

--- sneaky.sh
+++ sneaky.sh
@@ -1,34 +1,86 @@
 #!/bin/bash
 
-initial_sleep_seconds="$1"
-n_shots="$2"
-seconds_between_shots="$3"
-active_window_only="$4"
+params=""
+
+initial_sleep_seconds=90
+n_shots=100
+seconds_between_shots=60
+active_window_only=0
+
+while (( "$#" )); do
+  case $1 in
+    -s | --initial-sleep-seconds)
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ] && [[ $2 =~ ^-?[0-9]+$ ]]; then
+        initial_sleep_seconds="$2"
+        shift
+      else
+        echo "Error: Argument for $1 is missing or invalid" >&2
+        exit 1
+      fi
+      ;;
+    -w | --active-window-only)
+      active_window_only=1
+      shift
+      ;;
+    -n | --number-of-shots)
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ] && [[ $2 =~ ^-?[0-9]+$ ]]; then
+        n_shots="$2"
+        shift
+      else
+        echo "Error: Argument for $1 is missing or invalid" >&2
+        exit 1
+      fi
+      ;;
+    -d | --delay-between-shots)
+      if [ -n "$2" ] && [ "${2:0:1}" != "-" ] && [[ $2 =~ ^-?[0-9]+$ ]]; then
+        seconds_between_shots="$2"
+        shift
+      else
+        echo "Error: Argument for $1 is missing or invalid" >&2
+        exit 1
+      fi
+      ;;
+    -*=)
+      echo "Error: Unsupported flag $1" >&2
+      exit 1
+      ;;
+    *) 
+      params="$params $1"
+      shift
+      ;;
+  esac
+done
+
+eval set -- "$params"
+
+#initial_sleep_seconds="$1"
+#n_shots="$2"
+#seconds_between_shots="$3"
+#active_window_only="$4"
 
 printf "\n**** Welcome sneaky little one! ****\n\n"
-
-if [ -z "$4" ]; then
-  printf "Using default values. To set your own cancel and run the following instead\n\n"
-  printf "*** ./sneaky.sh initial_sleep_in_seconds n_shots time_between_shots_in_seconds true ***\n\n"
-  printf "or for multiple monitors: \n\n *** ./sneaky.sh initial_sleep_in_seconds n_shots time_between_shots_in_seconds false ***"
-  initial_sleep_seconds="90"
-  n_shots="100"
-  seconds_between_shots="60"
-  active_window_only="true"
-fi
 
 printf "Initial sleep time set to %d seconds,\n" "$initial_sleep_seconds"
 printf "taking %d screenshots,\n" "$n_shots"
 printf "time between shots set to %d." "$seconds_between_shots"
-if [ $active_window_only == "true" ]; then
+if [ $active_window_only == 1 ]; then
   printf "\nTaking screenshots of the active window only."
 fi
 
-printf "\n\nLet's go!\n\n"
+printf "\n\nTo change this values use the following options:\n\n"
+printf "\t-%s \t--%s \t%s\n" "s" "initial-sleep-seconds" "sets initial time to wait before starting"
+printf "\t-%s \t--%s \t%s\n" "n" "number-of-shots" "sets the number of screenshots to take"
+printf "\t-%s \t--%s \t%s\n" "d" "delay-between-shots" "sets the time in seconds to wait between screenshots"
+printf "\t-%s \t--%s \t%s\n\n" "w" "active-window-only" "this flag sets it to shoot only the active window"
+printf "example: to wait a minute, then take 5 screenshots of the active window"
+printf "waiting 10 seconds between shots call\n"
+printf " ./sneaky.sh -%s -%s 60 -%s 5 -%s 10" "w" "s" "n" "d"  
+
+printf "\n\nStarting...\n\n"
 printf "Sleeping for %d seconds..." "$initial_sleep_seconds"
 sleep "$initial_sleep_seconds"
 
-for i in $(seq $n_shots); do 
+for i in $(seq "$n_shots"); do 
   printf "Sleeping for %d seconds... \nTaking shot number %d of %d after my nap\n\n" \
    "$seconds_between_shots" "$i" "$n_shots"
   if [ $active_window_only == "true" ]; then
@@ -37,3 +89,6 @@ for i in $(seq $n_shots); do
    gnome-screenshot -B -d "$seconds_between_shots"
   fi
 done
+
+printf "Done!\n\n"
+
